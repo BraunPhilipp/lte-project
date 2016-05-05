52b57d20796558dca3610901e05ccc11440e1616
Philipp Braun <braunphilipp@protonmail.com>
2016-05-05 17:14:01 +0200

    Wasted

--- main.m
+++ main.m
@@ -1,15 +1,14 @@
 
-% Generate 4 Base Stations %
+% Generate 4 Base Stations
 b(1) = base_station(1, 61, [10, 10]);
 b(2) = base_station(1, 61, [10, 20]);
 b(3) = base_station(1, 61, [20, 20]);
 b(4) = base_station(1, 61, [20, 10]);
 
-% Seperate Coordinates %
-x = []; y = [];
-for k = 1:4
-    x = [x, b(k).pos(1)];
-    y = [y, b(k).pos(2)];
+% Generate 10 Random Users
+for i = 1:10
+    a(i) = user_entity(i, [randi([0 30], 1, 2)]);
 end
 
-scatter(x,y)
+a(1).distance(b(1))
+% Total waste of time will try some other day
--- user_entity.m
+++ user_entity.m
@@ -9,10 +9,11 @@ classdef user_entity
             obj.id = id_attr;
             obj.pos = pos_attr;
         end
-        function dist = distance(base_station_obj)
+        
+        function distance(base_station_obj)
             % Calculate Distance between Base Station and User Entity %
             tmp_pos = obj.pos - base_station_obj.pos;
-            dist = tmp_pos(1) / cos(atand(tmp_pos(2)/tmp_pos(1)));
+            tmp_pos(1) / cos(atand(tmp_pos(2)/tmp_pos(1)))
         end
    end
 end
