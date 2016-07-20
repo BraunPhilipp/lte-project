9273d954459a61bc8247341fcb2dc06c947b064d
Philipp Braun <braunphilipp@protonmail.com>
2016-07-20 22:53:07 +0200

    Channel Presentation Update

--- Simulator/main.m
+++ Simulator/main.m
@@ -43,46 +43,46 @@ cu = central_unit(1,ue,bs);
 % Create TBS
 TBS_obj = TBS('TBS.xls');
 
-%% Throughput Calculation
-% Dynamic Point Selection
-
-x_values = [];
-
-for j = 1:1000
-    % Generate coordinates of basestations
-    coordinates = helpers.calc_coordinates();
-    % Generate basestations according to the coordinats
-    [num_of_bs,~] = size(coordinates);
-    for i = 1:num_of_bs
-        bs(i) = base_station(i, coordinates(i,:), 61, params.num_subcarrier, 2000000000, 1400000, params.num_subcarrier, randi([8,16]));
-    end
-    
-    % Generate 32 Random Users
-    for i = 1:params.num_users
-        ue(i) = user_entity(i, randi([0 params.space_size], 1, 2), -135, randi([1,4]));
-    end
-
-    % Initialize Central Unit
-    cu = central_unit(1,ue,bs);
-
-    % Create TBS
-    TBS_obj = TBS('TBS.xls');
-    
-    cu.map_users_dps();
-
-    thrput = 0.0;
-    for i = 1:length(bs)
-        cu.base_list(i).scheduling();
-        cu.base_list(i).modulation(TBS_obj.TBs);
-        cu.base_list(i).beamforming();
-        thrput = thrput + cu.base_list(i).bhaul;
-    end
-
-    dps_thrput = thrput;
-    x_values = [x_values thrput];
-    %cu.draw(1);
-    j
-end
+% %% Throughput Calculation
+% % Dynamic Point Selection
+% 
+% x_values = [];
+% 
+% for j = 1:1000
+%     % Generate coordinates of basestations
+%     coordinates = helpers.calc_coordinates();
+%     % Generate basestations according to the coordinats
+%     [num_of_bs,~] = size(coordinates);
+%     for i = 1:num_of_bs
+%         bs(i) = base_station(i, coordinates(i,:), 61, params.num_subcarrier, 2000000000, 1400000, params.num_subcarrier, randi([8,16]));
+%     end
+%     
+%     % Generate 32 Random Users
+%     for i = 1:params.num_users
+%         ue(i) = user_entity(i, randi([0 params.space_size], 1, 2), -135, randi([1,4]));
+%     end
+% 
+%     % Initialize Central Unit
+%     cu = central_unit(1,ue,bs);
+% 
+%     % Create TBS
+%     TBS_obj = TBS('TBS.xls');
+%     
+%     cu.map_users_dps();
+% 
+%     thrput = 0.0;
+%     for i = 1:length(bs)
+%         cu.base_list(i).scheduling();
+%         cu.base_list(i).modulation(TBS_obj.TBs);
+%         cu.base_list(i).beamforming();
+%         thrput = thrput + cu.base_list(i).bhaul;
+%     end
+% 
+%     dps_thrput = thrput;
+%     x_values = [x_values thrput];
+%     %cu.draw(1);
+%     j
+% end
 
 % for j = 1:1000
 %     % Generate coordinates of basestations
@@ -124,7 +124,7 @@ end
 %cs_thrput
 %dps_thrput
 
-histfit(x_values);
+%histfit(x_values);
 
 
 %% Simulate Transmission
@@ -140,3 +140,7 @@ histfit(x_values);
 %     end
 %     cu.draw(1);
 % end
+
+ch = channel();
+ch.ray_chan();
+
